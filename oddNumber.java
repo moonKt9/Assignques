21d46380ed6691cafc78d31417c5c02c3b7ef9f9
Kritika <kritistarsmind@gmail.com>
2022-04-29 19:29:23 +0530

    Added:Assignques

---
+++ oddNumber.java
@@ -0,0 +1,13 @@
+package question1;
+
+public class oddNumber {
+	//print the odd number from 1 to 99
+	public static void main(String args[]) {
+		System.out.println("Odd number are :");
+		for(int i=1; i<=100;i++) {
+			if(i%2 !=0)
+				System.out.println(i+"");
+		}
+	}
+
+}
